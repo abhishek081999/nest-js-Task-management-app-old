df8c89a22d7cd3b811e9229875d1fe3643bd07ff
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-08-25 16:26:55 +0530

    Configuration management 25/08/2022

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
-import { Logger } from '@nestjs/common'
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
